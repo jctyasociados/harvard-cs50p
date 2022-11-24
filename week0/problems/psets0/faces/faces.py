08ef4dc1974b56a8074f1d370cdf8060318bfeba
Javier Cristobal <jctyasociados@gmail.com>
2022-11-24 18:34:16 +0100

    faces.py

---
+++ week0/problems/psets0/faces/faces.py
@@ -0,0 +1,12 @@
+def main():
+    """ Python script that converts emoticons to smily emoji and to frowny emoji """
+    str_to_convert = input("Input string containing smiley or frowney face: ").strip()
+    print(convert(str_to_convert))
+
+
+def convert(str_to_convert):
+    """ Function that converts emoticons into emojis """
+    converted_str = str_to_convert.replace(":(", "🙁").replace(":)", "🙂")
+    return converted_str
+    
+main()
--- week0/psets0/psets0/indoor/indoor.py
+++
@@ -1,5 +0,0 @@
-""" Script that take a string from the user and convert it to lowercase """
-
-str_to_convert = input("Enter string to convert to lowercase: \n")
-str_to_convert = str_to_convert.lower()
-print(str_to_convert)
