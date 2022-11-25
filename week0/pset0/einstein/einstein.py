f17ef2ab7ba0d7f7a79f0003353f8ec78fe5ace3
Javier Cristobal <jctyasociados@gmail.com>
2022-11-25 12:53:31 +0100

    einstein.py

--- week0/pset0/einstein/einstein.py
+++ week0/pset0/einstein/einstein.py
@@ -3,10 +3,11 @@
 def main():
     input_mass = input("Enter a Mass in kilograms: ").strip()
     print(f"Energy: {convert(int(input_mass))}")
-
+    
 def convert(input_mass):
     """ Convert Mass to Energy """
     energy = input_mass * (300000000**2)
     return energy
-
+    
 main()
+
