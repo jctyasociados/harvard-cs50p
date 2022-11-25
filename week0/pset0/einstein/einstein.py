58e3932f296de5422472c331435e53a10c4d01c7
Javier Cristobal <jctyasociados@gmail.com>
2022-11-25 13:15:20 +0100

    einstein.py

--- week0/pset0/einstein/einstein.py
+++ week0/pset0/einstein/einstein.py
@@ -1,8 +1,10 @@
 """ Python script to convert mass to energy according to Einstein's formula energy = mass * speed_of_light(squared) """
 
+
 def main():
     input_mass = input("Enter a Mass in kilograms: ").strip()
     print(f"Energy: {convert(int(input_mass))}")
+    
     
 def convert(input_mass):
     """ Convert Mass to Energy """
@@ -10,3 +12,4 @@ def convert(input_mass):
     return energy
     
 main()
+
