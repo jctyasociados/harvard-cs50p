8db6322904c9c43d38351e2be35c26199bcf0078
Javier Cristobal <jctyasociados@gmail.com>
2022-11-25 14:59:51 +0100

    tip.py

--- week0/pset0/tip/tip.py
+++ week0/pset0/tip/tip.py
@@ -7,15 +7,14 @@ def main():
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-""" Function that converts dollars to floating point with two decimals """
-
 
 def dollars_to_float(d):
+  """ Function that converts dollars to floating point with two decimals """
   return float('%.2f' % float(d.strip("$")))
 
 
-""" Function that converts tip given in percentage to floating point number with two decimals """
 def percent_to_float(p):
+  """ Function that converts tip given in percentage to floating point number with two decimals """
   tip = float(p.strip("%"))
   return float('%.2f' % (tip / 100))
 
